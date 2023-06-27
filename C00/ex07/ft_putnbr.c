26db0751dfe210fd81a69cd13b3d6e781b71e872
lucAsC87 <lucas2187@gmail.com>
2023-06-27 13:50:32 +0200

    pushing ex07

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	char	number[10];
+	int	i;
+
+	i = 0;
+	if (nb == 0)
+		write(1, "0", 1);
+	if (nb < 0)
+	{
+		nb *= -1;
+		write(1, "-", 1);
+	}
+	while (nb > 0)
+	{
+		number[i] = (nb % 10) + 48;
+		nb /= 10;
+		i++;
+	}
+	while (i > 0)
+	{
+		write(1, &number[i-1], 1);
+		i--;
+	}
+}

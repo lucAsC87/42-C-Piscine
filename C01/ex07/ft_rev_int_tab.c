d011d8007ce64965ea169c391369dd0f12458d8f
lucAsC87 <lucas2187@gmail.com>
2023-07-05 16:00:48 +0200

    pushing ex07

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,22 @@
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	newTab[size];
+
+	i = 0;
+	j = size - 1;
+	while (i < size)
+	{
+		newTab[i] = tab[j];
+		i++;
+		j--;
+	}
+	k = 0;
+	while (k < size)
+	{
+		tab[k] = newTab[k];
+		k++;
+	}
+}

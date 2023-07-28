67c214bf091c4e2dce9f9efd54d1cb6cccf6657f
lucAsC87 <lucas2187@gmail.com>
2023-07-28 10:29:42 +0200

    pushing correct version of ex00 and ex01

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -1,31 +1,9 @@
-#include <stdio.h>
-
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
-	int	j;
-	int	sum_s1;
-	int	sum_s2;
 
 	i = 0;
-	j = 0;
-	sum_s1 = 0;
-	sum_s2 = 0;
-	while (s1[i] != '\0')
-	{
-		sum_s1+=s1[i];
+	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
 		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		sum_s2+=s2[j];
-		j++;
-	}
-	
-	if (sum_s1 == sum_s2)
-		return (0);
-	else if (sum_s1 < sum_s2)
-		return (-1);
-	else
-		return (1);
+	return s1[i] - s2[i];
 }

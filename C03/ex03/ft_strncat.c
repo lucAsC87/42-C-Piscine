79df06743aa0fb80ec0f64a037fa3b1dd1871fac
lucAsC87 <lucas2187@gmail.com>
2023-08-03 14:49:47 +0200

    pushing ex03

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,19 @@
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && nb > 0)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		nb--;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

3bb34279dd302f172f9a4699e554e95c5dd77844
lucAsC87 <lucas2187@gmail.com>
2023-08-10 12:30:55 +0200

    pushin ex03 & ex04

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,9 @@
+int	ft_recursive_power(int nb, int power)
+{
+	if (nb == 0 && power == 0)
+		return (1);
+	if (power == 1)
+		return (nb);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
